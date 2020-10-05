846321e8402fd8f13d4a6800c2dd14aa65c607cf
Magicxavi <rolfpeterkesche@gmail.com>
2020-10-05 19:03:40 +0300

    blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_kenzo.mk
+    $(LOCAL_DIR)/bliss_kenzo.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_kenzo-userdebug \
-    havoc_kenzo-eng
+    bliss_kenzo-userdebug \
+    bliss_kenzo-eng
--- bliss_kenzo.mk
+++ bliss_kenzo.mk
@@ -21,21 +21,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/kenzo/device.mk)
 
 # Inherit some common BlissOS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := havoc_kenzo
+PRODUCT_NAME := bliss_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
 
 IS_PHONE := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Use the latest approved GMS identifiers unless running a signed build
@@ -54,9 +52,8 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 USE_CCACHE=0
 
-# Havoc build type
-HAVOC_BUILD_TYPE := Official
-ro.havoc.maintainer := Magicxavi
+BLISS_BUILDTYPE=OFFICIAL
+BLISS_MAINTAINER=Magicxavi
 
 PRODUCT_PACKAGES += \
     AdvancedControls \
